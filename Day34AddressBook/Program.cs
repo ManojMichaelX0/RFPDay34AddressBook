062ef50e235c98889b837f990b29a99fa4f522fa
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-04-08 18:39:48 +0530

    added UC-22 Read Data From Table and Write to Csv and Json

--- Day34AddressBook/Program.cs
+++ Day34AddressBook/Program.cs
@@ -10,20 +10,23 @@ namespace Day34AddressBook
             AddressBookRepo repo = new AddressBookRepo();
             //UC 20
             AddressBookModel model = new AddressBookModel();
+            //Call One Method At a Time, Comment Other Methods While Calling the Required Method
 
-            model.FirstName = "Eren";
-            model.LastName = "Jeager";
-            model._address = "25-4-710";
-            model.City = "Wall Maria";
-            model._State = "Anime";
-            model.Zip = 400504;
-            model.PhoneNumber = "8106529025";
-            model.email = "erenjeager@gmail.com";
-            model.RElationType = "Brother";
-            model.StartDate = DateTime.Now;
-            repo.AddDetials(model);
 
-            //Call One Method At a Time Comment Other Method While Calling the Required Method
+
+            //We Cannot Enter Duplicate Data below Data Change the data below to not Violate Rules
+            //model.FirstName = "Eren";
+            //model.LastName = "Jeager";
+            //model._address = "25-4-710";
+            //model.City = "Wall Maria";
+            //model._State = "Anime";
+            //model.Zip = 400504;
+            //model.PhoneNumber = "8106529025";
+            //model.email = "erenjeager@gmail.com";
+            //model.RElationType = "Brother";
+            //model.StartDate = DateTime.Now;
+            //repo.AddDetials(model);
+
             //UC 16
             //repo.GetDetails();
 
@@ -39,6 +42,17 @@ namespace Day34AddressBook
             //UC 19
             //repo.Count(model);
 
+            //UC 22 - CSv
+            ReadTableAndWitetoCsv rwcsv = new ReadTableAndWitetoCsv();
+            rwcsv.GetCSv();
+            Console.WriteLine("\nCompleted Reading and Writing\n");
+            Console.WriteLine("\nReading Data From that Csv File\n");
+            rwcsv.ReadallLines();
+
+            //UC 22 - JSON
+            ReadingTableAndWritingtoJson rwjson= new ReadingTableAndWritingtoJson();
+            rwjson.GetJson();
+            Console.WriteLine("Read and Write it Completed to Json File");
             
 
 
---
+++ Day34AddressBook/ReadingTableAndWritingtoJson.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Day34AddressBook
+{
+    internal class ReadingTableAndWritingtoJson
+    {
+        public void GetJson()
+        {
+            string json=string.Empty;
+            string path = @"Data Source=localhost\SQLEXPRESS;Database=Address_Book;Trusted_Connection=True";
+            List<object> list = new List<object>();
+            using(SqlConnection connect = new SqlConnection(path))
+            {
+                connect.Open();
+                using (SqlCommand cmd = connect.CreateCommand())
+                {
+                    cmd.CommandText = "Select * From Address_Book_Table";
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Dictionary<string, object> records = new Dictionary<string, object>();
+                            for(int i=0; i<reader.FieldCount; i++)
+                            {
+                                records.Add(reader.GetName(i), reader[i]);
+                            }
+                            list.Add(records);
+                        }
+                    }
+                }
+            }
+            json=JsonConvert.SerializeObject(list);
+            using (StreamWriter sw = new StreamWriter(File.Create(@"D:\.net\Day 34 Address Book\Day34AddressBook\TableData_TO_JsonFile.json.")))
+            {
+                sw.Write(json);
+            }
+            ReadallLines();
+            void ReadallLines()
+            {
+                string path = @"D:\.net\Day 34 Address Book\Day34AddressBook\TableData_TO_JsonFile.json";
+                string[] lines;
+                lines = File.ReadAllLines(path);
+                foreach (var l in lines)
+                {
+                    Console.WriteLine(l);
+                }
+            }
+        }
+        
+
+    }
+
+
+}
