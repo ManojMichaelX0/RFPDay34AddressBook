322a12715d74b5f497be3f5b1f2df9c2786a1576
ManojMichaelX0 <98898332+ManojMichaelX0@users.noreply.github.com>
2022-04-08 07:07:22 +0530

    Update AddressBookStoredProcedure.sql

--- AddressBookStoredProcedure.sql
+++ AddressBookStoredProcedure.sql
@@ -1,18 +1,18 @@
-USE [Address_Book]
-GO
-
 CREATE Procedure [dbo].[SPAddressBook]
 (
-@FirstName varchar,
-@LastName varchar,
-@_address varchar,
-@City varchar,
-@_State varchar,
+@FirstName varchar(50),
+@LastName varchar(50),
+@_address varchar(50),
+@City varchar(50),
+@_State varchar(50),
 @Zip int ,
-@PhoneNumber varchar,
-@email varchar,
-@RelationType varchar
+@PhoneNumber varchar(12),
+@email varchar(50),
+@RElationType varchar(20),
+@StartDate datetime
 )
 As
-	
-
+Begin
+Insert into Address_Book_Table values(@FirstName,@LastName,@_address,@City,@_State,@Zip,@PhoneNumber,@email,@RElationType,@StartDate)
+SET NOCOUNT ON
+End
